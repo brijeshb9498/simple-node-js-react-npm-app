64a827568f73017a13645b4066d540fb17370b6e
Ubuntu <ubuntu@ip-172-31-12-142.ap-south-1.compute.internal>
2024-06-11 11:36:20 +0000

    update

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
+
+echo "Stopping the server..."
+pkill -f "react-scripts start"
+
